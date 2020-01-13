47b5830a6e9869f84ed875126f563a4b140c5693
Jackson Mo <jacksonmo@me.com>
2020-01-13 20:19:57 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -80,12 +80,15 @@ def adding_matz
           :known_for => "Unix",
           :languages => ["C"]
         },
-        :yukihiro_matsumoto => {
-          :known_for => "Ruby",
-          :languages => ["LISP", "C"]
+        #:yukihiro_matsumoto => {
+          #:known_for => "Ruby",
+          #:languages => ["LISP", "C"]
         }
      }
-
+  programmer_hash += :yukihiro_matsumoto => {
+          :known_for => "Ruby",
+          :languages => ["LISP", "C"]
+  }
     
 end
 
